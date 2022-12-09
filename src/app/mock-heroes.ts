1339debc273c53cd4752784065a1f4fb8f420aec
Catalin Ispas <70150418+catalinispas@users.noreply.github.com>
2022-12-09 14:17:04 +0200

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Winston' },
+  { id: 13, name: 'Bastion' },
+  { id: 14, name: 'Ana' },
+  { id: 15, name: 'Mercy' },
+  { id: 16, name: 'Reaper' },
+  { id: 17, name: 'Sigma' },
+  { id: 18, name: 'Ashe' },
+  { id: 19, name: 'Pharah' },
+  { id: 20, name: 'Orisa' },
+];
